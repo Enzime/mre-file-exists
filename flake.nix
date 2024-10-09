a2b4c173694707c307139e449f339ae6388dfc40
Michael Hoang <enzime@users.noreply.github.com>
2024-10-09 14:59:10 +1100

    Pin old nixos-unstable commit

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/ad416d066ca1222956472ab7d0555a6946746a80";
 
   outputs = { self, nixpkgs }: let
     pkgs = nixpkgs.legacyPackages.aarch64-linux;
